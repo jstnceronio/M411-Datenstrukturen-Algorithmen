623b8bfb3d6bb3853e294f1b25f8ff17127d3f2c
Justin Ceronio <justin.ceronio@bit.admin.ch>
2021-02-04 10:36:29 +0100

    split procedural/functional

--- datenstrukturen/src/main/java/AB411_02_collatz/Collatz.java
+++ datenstrukturen/src/main/java/AB411_02_collatz/Collatz.java
@@ -6,13 +6,34 @@ import java.io.InputStreamReader;
 
 public class Collatz {
     public static void main(String[] args) {
-        collatz();
+        // procedural
+        collatz_procedure(19);
     }
 
-    public static void collatz() {
-        try {
-            int i = 0;
+    public static void collatz_procedure(long n) {
+        // Counter
+        int i = 0;
 
+        // Confirm
+        System.out.println("Collatz-Berechnung für: " + n);
+        System.out.println("Prozedurale Berechnung: ");
+
+        // Process
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            i++;
+            System.out.print(n + ", ");
+        }
+        System.out.println("Fertig!");
+    }
+
+    /*
+    private static void collatz_functional() {
+        try {
             BufferedReader br = new BufferedReader(
                     new InputStreamReader(System.in));
 
@@ -25,12 +46,13 @@ public class Collatz {
                 } else {
                     n = 3 * n + 1;
                 }
-                i++;
-                System.out.println("Schritt " + i + ": " + n);
+                System.out.print(n + ", ");
             }
             System.out.println("Fertig!");
+
         } catch (IOException e) {
             System.out.println(e);
         }
     }
+     */
 }
